96a3ac5d361c670846d34f3988fcf3c23cf43d92
Abhishek Verma <68803785+oyeeabhishek@users.noreply.github.com>
2021-05-17 19:12:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pickle
 
 # Loading crop recommendation model
 
-crop_recommendation_model_path = 'D:\\New-Model\\RF_Model_pkl'
+crop_recommendation_model_path = 'https://github.com/oyeeabhishek/KrishiNanban/blob/main/RF_Model_pkl'
 crop_recommendation_model = pickle.load(open(crop_recommendation_model_path, 'rb'))
 
 
